3ea2d4973d029438b1a36417333a0ba5443417ea
laughingbiscuit <sean@laughingbiscuit.com>
2023-05-25 20:23:46 +0000

    poc: install devenv in pipeline

--- run.sh
+++ run.sh
@@ -6,8 +6,11 @@ set -e
 rm -rf target/
 git clone https://github.com/laughingbiscuit/bdd target/
 
+sudo apt-get install -y ruby-dev 
+sudo gem install bundler
+
 # merge
 cp -r features/ target/features/
 
 # run tests
-(cd target/ && bundle install && npm install && sh test.sh && npm run report)
+(cd target/ && bundle install && npm install && sh test.sh)
