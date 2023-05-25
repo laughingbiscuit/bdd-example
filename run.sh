04a41a67ed5e24951a595df10b4c7fd3152e3d62
laughingbiscuit <sean@laughingbiscuit.com>
2023-05-25 21:40:41 +0000

    add interactive flag for copy to warn when overwriting

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ rm -rf target/
 git clone https://github.com/laughingbiscuit/bdd target/
 
 # merge
-cp -r features/ target/features/
+cp -i -r features/ target/features/
 
 # env
 (cd target && docker build -t blah .)
